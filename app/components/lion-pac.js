22a4ffda0b64980ea78cf03fb0e951ec9d4cd456
Mayank Mishra <mayank.mishra@traxtech.com>
2016-11-12 08:56:29 -0700

    Created grid like functionality

--- app/components/lion-pac.js
+++ app/components/lion-pac.js
@@ -6,24 +6,78 @@ export default Ember.Component.extend(KeyboardShortcuts, {
     this.drawCircle();
   },
 
-  drawCircle: function() {
+  x: 1,
+  y: 2,
+  squareSize: 40,
+
+
+
+  ctx: Ember.computed(function(){
     let canvas = document.getElementById("myCanvas");
     let ctx = canvas.getContext("2d");
-    let x = 50;
-    let y = 100;
-    let radius = 20;
+    return ctx;
+  }),
+
+  drawCircle: function() {
+    let ctx = this.get('ctx');
+    let x = this.get('x');
+    let y = this.get('y');
+    let squareSize = this.get('squareSize')/2;
+
+    let pixelX = (x+1/2) * squareSize;
+    let pixelY = (y+1/2) * squareSize;
 
     ctx.fillStyle = '#000';
     ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+    ctx.arc(pixelX, pixelY, squareSize/2, 0, Math.PI * 2, false);
     ctx.closePath();
     ctx.fill();
   },
 
-  keyboardShortcuts: {
-    up: function() { console.log('up'); },
-    down: function() { console.log('down'); },
-    left: function () {console.log('left');},
-    right: function () {console.log('right');},
+  moveLionPac: function(direction, amount) {
+    this.incrementProperty(direction, amount);
+
+    if (this.collidedWithBorder()) {
+      this.decrementProperty(direction, amount)
+    }
+
+    this.clearScreen();
+    this.drawCircle();
   },
+
+  keyboardShortcuts: {
+    up: function() { this.moveLionPac('y', -1); },
+    down: function() { this.moveLionPac('y',  1) },
+    left: function () { this.moveLionPac('x', -1); },
+    right: function () { this.moveLionPac('x', 1); },
+  },
+
+  screenWidth: 20,
+  screenHeight: 15,
+
+  screenPixelWidth: Ember.computed(function() {
+    return this.get('screenWidth') * this.get('squareSize');
+  }),
+  screenPixelHeight: Ember.computed(function() {
+    return this.get('screenHeight') * this.get('squareSize');
+  }),
+
+  clearScreen: function() {
+    let ctx = this.get('ctx');
+    ctx.clearRect(0, 0, this.get('screenPixelWidth'), this.get('screenPixelHeight'))
+  },
+
+  collidedWithBorder: function() {
+    let x = this.get('x');
+    let y = this.get('y');
+    let screenHeight = this.get('screenHeight');
+    let screenWidth = this.get('screenPixelWidth');
+
+    let pacOutOfBounds = x < 0 ||
+                         y < 0 ||
+                         x >= screenWidth ||
+                         y >= screenHeight
+    return pacOutOfBounds
+  },
+
 });
